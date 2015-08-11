7cb1889a19c3ccfda9cae9eba86b41014a6f46da
Taehoon Moon <panarch@kaist.ac.kr>
2015-08-11 09:37:03 +0000

    Added index FlexibleLayout example and refined codes

--- src/index.js
+++ src/index.js
@@ -48,140 +48,161 @@ var CanvasSurface = require('../surfaces/CanvasSurface');
 var ContainerSurface = require('../surfaces/ContainerSurface');
 var StateModifier = require('../modifiers/StateModifier');
 var SequentialLayout = require('../views/SequentialLayout');
+var FlexibleLayout = require('../views/FlexibleLayout');
 var GridLayout = require('../views/GridLayout');
 var Utility = require('../utilities/Utility');
 var Easing = require('../transitions/Easing');
 
 var ctx = Engine.createContext();
 
-var modifier = new StateModifier({
-    size: [undefined, 100],
-    align: [0.7, 0.5],
-    origin: [0.5, 0.5],
-    proportions: [0.2, 1],
-    opacity: 0.5
-    //transform: Transform.thenMove(Transform.rotateZ(Math.PI / 3), [200, 50, 0])
-});
+/*
+ * StateModifier
+ */
+function addStateModifier() {
+    var modifier = new StateModifier({
+        size: [undefined, 100],
+        align: [0.7, 0.5],
+        origin: [0.5, 0.5],
+        proportions: [0.2, 1],
+        opacity: 0.5
+        //transform: Transform.thenMove(Transform.rotateZ(Math.PI / 3), [200, 50, 0])
+    });
 
-modifier.setAlign([0.5, 0.5], { duration: 2000, curve: Easing.inExpo });
-modifier.setTransform(Transform.thenMove(Transform.rotateZ(Math.PI / 3), [200, 50, 0]), { duration: 2000, curve: Easing.inExpo });
+    modifier.setAlign(
+        [0.5, 0.5],
+        { duration: 1000, curve: Easing.inExpo },
+        function() {
+            modifier.setTransform(
+                Transform.thenMove(Transform.rotateZ(Math.PI / 3), [50, 50, 0]),
+                { duration: 800, curve: Easing.inExpo }
+            );
+        }
+    );
 
-var surface = new Surface({
-    content: 'HI',
-    properties: {
-        backgroundColor: 'red',
-        color: 'white',
-        textAlign: 'center',
-        fontSize: '40px',
-        borderRadius: '10px'
-    }
-});
+    var surface = new Surface({
+        content: 'HI',
+        properties: {
+            backgroundColor: 'red',
+            color: 'white',
+            textAlign: 'center',
+            fontSize: '40px',
+            borderRadius: '10px'
+        }
+    });
 
-var container = new ContainerSurface({
-    size: [200, 200],
-    properties: {
-        border: '1px',
-        backgroundColor: '#eaeaea'
-    }
-});
+    ctx.add(modifier).add(surface);
+}
 
-var m2 = new Modifier({
-    size: [30, 30],
-    align: [0.1, 0],
-    origin: [0, 0],
-    transform: Transform.translate(0, 10)
-});
+/*
+ * ContainerSurface
+ */
+function addContainerSurface() {
+    var container = new ContainerSurface({
+        size: [200, 200],
+        properties: {
+            border: '1px',
+            backgroundColor: '#eaeaea'
+        }
+    });
 
-var s2 = new Surface({
-    properties: {
-        backgroundColor: 'blue'
-    }
-});
+    var m2 = new Modifier({
+        size: [30, 30],
+        align: [0.1, 0],
+        origin: [0, 0],
+        transform: Transform.translate(0, 10)
+    });
 
-var m3 = new Modifier({
-    size: [40, 40],
-    align: [0.9, 0.9],
-    origin: [1, 1],
-    transform: Transform.rotateZ(Math.PI / 6)
-});
+    var s2 = new Surface({
+        properties: {
+            backgroundColor: 'blue'
+        }
+    });
 
-var s3 = new Surface({
-    properties: {
-        backgroundColor: 'green'
-    }
-});
+    var m3 = new Modifier({
+        size: [40, 40],
+        align: [0.9, 0.9],
+        origin: [1, 1],
+        transform: Transform.rotateZ(Math.PI / 6)
+    });
+
+    var s3 = new Surface({
+        properties: {
+            backgroundColor: 'green'
+        }
+    });
+
+    container.add(m2).add(s2);
+    container.add(m3).add(s3);
+    ctx.add(container);
+}
 
 /**
- * Sequential Layout
+ * SequentialLayout
  */
-var sequentialModifier = new Modifier({
-    size: [200, 80],
-    align: [0.5, 0.5],
-    origin: [0.5, 0.5]
-});
+function addSequentialLayout() {
+    var modifier = new Modifier({
+        size: [200, 80],
+        align: [0, 0],
+        origin: [0, 0],
+        transform: Transform.translate(100, 210)
+    });
 
-var sequentialLayout = new SequentialLayout({
-    direction: Utility.Direction.X
-});
+    var layout = new SequentialLayout({
+        direction: Utility.Direction.X
+    });
 
-var s4 = new ImageSurface({
-    size: [80, 80],
-    content: './images/famous_logo.png'
-});
+    var item1 = new ImageSurface({
+        size: [80, 80],
+        content: './images/famous_logo.png'
+    });
 
-var s5 = new Surface({
-    size: [120, 80],
-    content: 'Example',
-    properties: {
-        textAlign: 'center',
-        lineHeight: '80px',
-        backgroundColor: '#eaeaea'
-    }
-});
+    var item2 = new Surface({
+        size: [120, 80],
+        content: 'Example',
+        properties: {
+            textAlign: 'center',
+            lineHeight: '80px',
+            backgroundColor: '#eaeaea'
+        }
+    });
 
-sequentialLayout.sequenceFrom([s4, s5]);
+    layout.sequenceFrom([item1, item2]);
+    ctx.add(modifier).add(layout);
+}
 
 /*
  * CanvasSurface
  */
-var m5 = new Modifier({
-    size: [100, 100],
-    align: [0, 0],
-    origin: [0, 0],
-    transform: Transform.translate(0, 200)
-});
+function addCanvasSurface() {
+    var modifier = new Modifier({
+        size: [100, 100],
+        align: [0, 0],
+        origin: [0, 0],
+        transform: Transform.translate(0, 200)
+    });
 
-var s6 = new CanvasSurface({
-    canvasSize: [200, 200],
-    properties: {
-        backgroundColor: '#aeaeae'
-    }
-});
+    var surface = new CanvasSurface({
+        canvasSize: [200, 200],
+        properties: {
+            backgroundColor: '#aeaeae'
+        }
+    });
 
-s6.on('deploy', function() {
-    // test drawing: white circle
-    var ctx = s6.getContext('2d');
-    var w = s6.getSize()[0];
-    ctx.beginPath();
-    ctx.arc(w, w, 30, -0.5 * Math.PI, 1.5 * Math.PI);
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 10;
-    ctx.stroke();
-});
+    surface.on('deploy', function() {
+        // test drawing: white circle
+        var ctx = surface.getContext('2d');
+        var w = surface.getSize()[0]; // half of canvas width
+        ctx.beginPath();
+        ctx.arc(w, w, 30, -0.5 * Math.PI, 1.5 * Math.PI);
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 10;
+        ctx.stroke();
+    });
 
-/*
- * GridLayout
- */
-var gridModifier = new Modifier({
-    size: [200, 200],
-    transform: Transform.translate(0, 300)
-});
+    ctx.add(modifier).add(surface);
+}
 
-var gridLayout = new GridLayout({
-    dimensions: [3, 4]
-});
-
-function getItem() {
+function getRandomColor() {
     var r = (Math.random() * 256) | 0;
     var g = (Math.random() * 256) | 0;
     var b = (Math.random() * 256) | 0;
@@ -190,24 +211,86 @@ function getItem() {
         .replace('{g}', g)
         .replace('{b}', b);
 
-    return new Surface({
-        properties: {
-            backgroundColor: color
-        }
+    return color;
+}
+
+/*
+ * GridLayout
+ */
+function addGridLayout() {
+    var gridModifier = new Modifier({
+        size: [200, 200],
+        transform: Transform.translate(0, 300)
     });
+
+    var gridLayout = new GridLayout({
+        dimensions: [3, 4]
+    });
+
+    function getItem() {
+        return new Surface({
+            properties: {
+                backgroundColor: getRandomColor()
+            }
+        });
+    }
+
+    var items = [];
+    for (var i = 0; i < 10; i++) {
+        items.push(getItem());
+    }
+
+    gridLayout.sequenceFrom(items);
+    ctx.add(gridModifier).add(gridLayout);
 }
 
-var items = [];
-for (var i = 0; i < 10; i++) {
-    items.push(getItem());
+/*
+ * FlexibleLayout
+ */
+function addFlexibleLayout() {
+    var modifier = new Modifier({
+        size: [200, 200],
+        transform: Transform.translate(200, 0)
+    });
+
+    var layout = new FlexibleLayout({
+        direction: Utility.Direction.X,
+        ratios: [true, 1, 2, 3, true]
+    });
+
+    function getItem() {
+        return new Surface({
+            properties: {
+                backgroundColor: getRandomColor()
+            }
+        });
+    }
+
+    var items = [
+        new Surface({
+            size: [20, undefined],
+            properties: {
+                backgroundColor: getRandomColor()
+            }
+        }),
+        getItem(),
+        getItem(),
+        getItem(),
+        new Surface({
+            size: [10, undefined],
+            properties: {
+                backgroundColor: getRandomColor()
+            }
+        })
+    ];
+
+    layout.sequenceFrom(items);
+    ctx.add(modifier).add(layout);
 }
 
-gridLayout.sequenceFrom(items);
-
-ctx.add(modifier).add(surface);
-container.add(m2).add(s2);
-container.add(m3).add(s3);
-ctx.add(container);
-ctx.add(sequentialModifier).add(sequentialLayout);
-ctx.add(m5).add(s6);
-ctx.add(gridModifier).add(gridLayout);
+addStateModifier();
+addContainerSurface();
+addSequentialLayout();
+addCanvasSurface();
+addGridLayout();
+addFlexibleLayout();
