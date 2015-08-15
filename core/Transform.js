5f3e864d9af0dd1c30a481a4d0379ac5207d59b2
Taehoon Moon <panarch@kaist.ac.kr>
2015-08-15 15:46:27 +0000

    Fixed Transform not to have illegal values

--- core/Transform.js
+++ core/Transform.js
@@ -20,45 +20,68 @@ Transform.prototype.getScale = function getScale() {
     return this._scale;
 };
 
+Transform.prototype.getValue = function getValue() {
+    return {
+        position: this._position.slice(),
+        rotation: this._rotation.slice(),
+        scale: this._scale.slice()
+    };
+};
+
 Transform.identity = new Transform();
 
-Transform.translate = function(x, y, z) {
+Transform._fill = function _fill(arr, defaultVal) {
+    for (var i = 0; i < arr.length; i++) {
+        var val = arr[i];
+        if (typeof val !== 'number' || isNaN(val))
+            arr[i] = defaultVal;
+    }
+};
+
+Transform.translate = function translate(x, y, z) {
     var transform = new Transform();
     transform._position = [x, y, z];
+    Transform._fill(transform._position, 0);
     return transform;
 };
 
 Transform.scale = function scale(x, y, z) {
     var transform = new Transform();
     transform._scale = [x, y, z];
+    Transform._fill(transform._scale, 1);
     return transform;
 };
 
 Transform.rotateX = function rotateX(theta) {
     var transform = new Transform();
-    transform._rotation = [theta, undefined, undefined];
+    transform._rotation = [theta, 0, 0];
+    Transform._fill(transform._rotation, 0);
     return transform;
 };
 
 Transform.rotateY = function rotateY(theta) {
     var transform = new Transform();
-    transform._rotation = [undefined, theta, undefined];
+    transform._rotation = [0, theta, 0];
+    Transform._fill(transform._rotation, 0);
     return transform;
 };
 
 Transform.rotateZ = function rotateZ(theta) {
     var transform = new Transform();
-    transform._rotation = [undefined, undefined, theta];
+    transform._rotation = [0, 0, theta];
+    Transform._fill(transform._rotation, 0);
     return transform;
 };
 
 Transform.thenMove = function thenMove(transform, position) {
     transform._position = position;
+    Transform._fill(transform._position, 0);
     return transform;
 };
 
 Transform.thenScale = function thenScale(transform, scale) {
     transform._scale = scale;
+    Transform._fill(transform._scale, 1);
     return transform;
 };
 
