d07d4c074cb736613725bf4b5fd7e51fac03fc8e
Taehoon Moon <panarch@kaist.ac.kr>
2015-08-15 18:28:38 +0000

    Replaced StateModifier transform animation handler to use Transitionable

--- modifiers/StateModifier.js
+++ modifiers/StateModifier.js
@@ -3,40 +3,38 @@
 'use strict';
 
 var DOMElement = require('famous/dom-renderables/DOMElement');
-var Position = require('famous/components/Position');
-var Rotation = require('famous/components/Rotation');
-var Scale = require('famous/components/Scale');
 var Align = require('famous/components/Align');
 var Origin = require('famous/components/Origin');
 var MountPoint = require('famous/components/MountPoint');
 var Size = require('famous/components/Size');
 var Opacity = require('famous/components/Opacity');
+var Transitionable = require('famous/transitions/Transitionable');
 
 var Surface = require('../core/Surface');
 var View = require('../core/View');
+var Transform = require('../core/Transform');
 
 function StateModifier(options) {
     View.apply(this, arguments);
 
+    this._compId = null;
     this._onLoadCallbacks = [];
+    this._transDict = {};
+    this._onTransition = false;
 
-    this.transform = options.transform ? options.transform : null;
-    this.opacity = options.opacity !== undefined ? options.opacity : null;
-    this.origin = options.origin ? options.origin : null;
-    this.align = options.align ? options.align : null;
+    this.transform = options.transform ? options.transform : Transform.identity;
+    this.opacity = options.opacity !== undefined ? options.opacity : 1;
+    this.origin = options.origin ? options.origin : [0, 0, 0];
+    this.align = options.align ? options.align : [0, 0, 0];
     this.size = options.size ? options.size : null;
     this.proportions = options.proportions ? options.proportions : null;
 
-    this.transformOptions = null;
     this.opacityOptions = null;
     this.originOptions = null;
     this.alignOptions = null;
     this.sizeOptions = null;
     this.proportionsOptions = null;
 
-    this._positionComp = null;
-    this._rotationComp = null;
-    this._scaleComp = null;
     this._alignComp = null;
     this._originComp = null;
     this._mountPointComp = null;
@@ -47,47 +45,47 @@ function StateModifier(options) {
 StateModifier.prototype = Object.create(View.prototype);
 StateModifier.prototype.constructor = StateModifier;
 
+/*
+ * enum
+ * Currently it only uses Transform...
+ */
+StateModifier.Type = {
+    TRANSFORM: 1
+};
+
 function _setComponentValue(component, value, options, callback) {
     component.set.apply(component, value.concat(options).concat(callback));
 }
 
 StateModifier.prototype.setTransform = function setTransform(transform, options, callback) {
+    var transitionable = new Transitionable(this.transform.getValue());
+
     var position = transform.getPosition();
     var rotation = transform.getRotation();
     var scale = transform.getScale();
 
-    while (position && position.length < 3) position.push(null);
-    while (rotation && rotation.length < 3) rotation.push(null);
-    while (scale && scale.length < 3) scale.push(1);
-
-    var called = false;
-    var _callback = callback;
-    callback = function() {
-        if (!called) {
-            called = true;
-            if (_callback) _callback();
-        }
-    };
+    function run() {
+        transitionable.set(transform.getValue(), options);
+        this._transDict[StateModifier.Type.TRANSFORM] = [transitionable, callback];
+        if (!this._onTransition)
+            this.node.requestUpdate(this._compId);
+    }
 
     if (this.node) {
-        if (position) _setComponentValue(this._positionComp, position, options, callback);
-        if (rotation) _setComponentValue(this._rotationComp, rotation, options, callback);
-        if (scale) _setComponentValue(this._scaleComp, scale, options, callback);
+        if (options)
+            run.call(this);
+        else {
+            this.node.setPosition.apply(this.node, position);
+            this.node.setRotation.apply(this.node, rotation);
+            this.node.setScale.apply(this.node, scale);
+        }
     }
     else if (options) {
-        this._onLoadCallbacks.push((function() {
-            this.transform = transform;
-            this.transformOptions = options;
-            if (position) _setComponentValue(this._positionComp, position, options, callback);
-            if (rotation) _setComponentValue(this._rotationComp, rotation, options, callback);
-            if (scale) _setComponentValue(this._scaleComp, scale, options, callback);
-        }).bind(this));
-
+        this._onLoadCallbacks.push(run.bind(this));
         return;
     }
 
     this.transform = transform;
-    this.transformOptions = options;
 };
 
 StateModifier.prototype.setOpacity = function setOpacity(opacity, options, callback) {
@@ -159,18 +157,42 @@ StateModifier.prototype.setProportions = function setProportions(proportions) {
     this.node.setProportionalSize.apply(this.node, proportions);
 };
 
+StateModifier.prototype._setTransform = function _setTransform(time, transitionable) {
+    var value = transitionable.get(time);
+    this.node.setPosition.apply(this.node, value.position);
+    this.node.setRotation.apply(this.node, value.rotation);
+    this.node.setScale.apply(this.node, value.scale);
+};
+
 StateModifier.prototype.setNode = function setNode(node) {
     this._setNode(node);
-    this.el = new DOMElement(node, {});
+    this.node.el = new DOMElement(node, {});
+
+    this._compId = this.node.addComponent({
+        onUpdate: (function(time) {
+            var item = this._transDict[StateModifier.Type.TRANSFORM];
+            if (!item) return;
+
+            var transitionable = item[0];
+            var callback = item[1];
+
+            this._setTransform(time, transitionable);
+            this._onTransition = transitionable.isActive();
+
+            if (this._onTransition)
+                node.requestUpdateOnNextTick(this._compId);
+            else {
+                delete this._transDict[StateModifier.Type.TRANSFORM];
+                if (callback) callback();
+            }
+        }).bind(this)
+    });
 
     var i;
     for (i = 0; i < this._queue.length; i++) {
         this._queue[i].setNode(this.node.addChild());
     }
 
-    this._positionComp = new Position(node);
-    this._rotationComp = new Rotation(node);
-    this._scaleComp = new Scale(node);
     this._alignComp = new Align(node);
     this._originComp = new Origin(node);
     this._mountPointComp = new MountPoint(node);
